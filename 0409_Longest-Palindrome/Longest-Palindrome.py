e774e47e8bdac7f42e499d3804b3125c20a8689d
Samuel Catania <69449284+samueljcatania@users.noreply.github.com>
2023-01-25 20:28:22 -0500

    Update Longest-Palindrome.py

--- 0409_Longest-Palindrome/Longest-Palindrome.py
+++ 0409_Longest-Palindrome/Longest-Palindrome.py
@@ -2,7 +2,6 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         dict = {}
         length = 0
-        input_length = 0
 
         for letter in s:
             dict[letter] = dict[letter] + 1 if letter in dict else 1
@@ -10,6 +9,4 @@ class Solution:
             if dict[letter] % 2 == 0:
                 length += 2
 
-            input_length += 1
-
-        return length + 1 if input_length > length else length
+        return length + 1 if len(s) > length else length
